ccdf3fdf88e09b26f70668a1b702b1752494a51b
Zlatan Cilic <zlatancilic@hotmail.com>
2015-05-25 19:25:39 +0200

    novosti iz baze bez dumpa

--- novosti.php
+++ novosti.php
@@ -1,4 +1,39 @@
 <?php
+
+    $con = new PDO("mysql:dbname=elitech;host=localhost;charset=utf8", "root", "dbpass");
+    $con->exec("set names utf8");
+    $lista_vijesti = $con->query("select id, naslov, UNIX_TIMESTAMP(vrijeme) vrijeme2, autor, tekst, urlSlike, detaljno from novost order by vrijeme desc");
+    if (!$lista_vijesti) {
+          $error = $con->errorInfo();
+          print $error[2];
+          exit();
+     }
+
+     foreach($lista_vijesti as $vijest) {
+        $id = $vijest['id'];
+        $naslov = $vijest['naslov'];
+        $datum = date("d.m.Y. h:i:s", $vijest['vrijeme2']);
+        $autor = $vijest['autor'];
+        $tekst = $vijest['tekst'];
+        $slika = $vijest['urlSlike'];
+        $detaljno = $vijest['detaljno'];
+
+        $linkDetaljno = "";
+        if($detaljno != "" && $detaljno != null) {
+            $linkDetaljno = "Detaljnije";
+        }
+
+        print("<div class = 'item'>
+            <img src ='".htmlentities($slika, ENT_QUOTES)."' alt = 'pr_slika'>
+            <h3>".htmlentities($naslov, ENT_QUOTES)."</h3>
+            <p class = 'datum'> Datum: ".htmlentities($datum, ENT_QUOTES)."</p>
+            <p class = 'autor'> Autor: ".htmlentities($autor, ENT_QUOTES)."</p>
+            <p class = 'sazetak'>".htmlentities($tekst, ENT_QUOTES)."<a onclick='dajNovost(".$id.")'>".$linkDetaljno."</a></p>
+            </div>"); 
+     }
+
+
+    /*
     $files = glob('novosti/*.txt', GLOB_BRACE);
     $niz = array();
     foreach($files as $file) {
@@ -69,5 +104,5 @@
             <p class = 'autor'> Autor: ".htmlentities($autor, ENT_QUOTES)."</p>
             <p class = 'sazetak'>".htmlentities($tekst, ENT_QUOTES)."<a onclick='dajNovost(".json_encode($novost).")'>".$linkDetaljno."</a></p>
             </div>");
-    }
+    }*/
 ?>
